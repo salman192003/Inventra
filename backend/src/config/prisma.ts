12a754a89dfe65f8dfd465c0995580e9c8e51814
Muhammad Salman <26100177@lums.edu.pk>
2026-03-17 23:12:27 +0500

    connection string updated

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -6,9 +6,18 @@ declare global {
 }
 
 // Prevent multiple Prisma Client instances in development (hot reload)
+
+const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres.bapjpnvndyfvsbhfrpyy:LZ%23%3FFka%2BC_Yr8%2Aj@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres?pgbouncer=true';
+const directUrl = process.env.DIRECT_URL || 'postgresql://postgres.bapjpnvndyfvsbhfrpyy:LZ%23%3FFka%2BC_Yr8%2Aj@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres';
+
 const prisma: PrismaClient =
   global.__prisma ??
   new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl
+      },
+    },
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
